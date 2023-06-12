94cc32c93b55c85048914e4afc5207c478cc19df
Wagner Mengue <dev.mengue@gmail.com>
2023-06-11 21:07:35 -0300

    created command to sync with es -- not working :(

--- src/commands/fixtures.command.ts
+++ src/commands/fixtures.command.ts
@@ -1,8 +1,38 @@
+import  {default as chalk} from 'chalk';
+import {MicroNodeApplication} from "../application";
+import * as config from '../config';
+import {Esv7DataSource} from "../datasources";
+import {Client} from "es7";
+
 export class FixturesCommand{
     static command = 'fixtures';
     static description = 'Fixtures data in ElasticSearch';
 
+    app: MicroNodeApplication;
+
     async run () {
         console.log("running fixtures");
+        await this.bootApp();
+        console.log(chalk.red('Delete all documents'));
+        await this.deleteAllDocuments();
+    }
+
+    private async bootApp() {
+        this.app = new MicroNodeApplication(config);
+        await this.app.boot();
+    }
+
+    private async deleteAllDocuments() {
+        const datasource : Esv7DataSource = this.app.getSync<Esv7DataSource>('datasources.esv7');
+        // @ts-ignore
+        const index = datasource.adapter.settings.index;
+        // @ts-ignore
+        const client: Client = datasource.adapter.db;
+        await client.delete_by_query({
+            index,
+            body: {
+                query: {match_all: {}}
+            }
+        })
     }
 }
--- src/console.ts
+++ src/console.ts
@@ -1,3 +1,4 @@
+import './bootstrap';
 import * as commands from './commands';
 import {default as chalk} from 'chalk';
 
